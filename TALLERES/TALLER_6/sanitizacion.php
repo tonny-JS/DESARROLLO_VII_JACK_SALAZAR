5ad278ae8181e74e526f9a241f98d181bc728818
tonny-JS <jack.salazar@utp.ac.pa>
2025-10-05 22:54:18 -0600

    TALLER_6 - Paso 3: Implementación de funciones de sanitización de datos

--- TALLERES/TALLER_6/sanitizacion.php
+++ TALLERES/TALLER_6/sanitizacion.php
@@ -1,6 +1,6 @@
 <?php
 function sanitizarNombre($nombre) {
-    return filter_var(trim($nombre), FILTER_SANITIZE_STRING);
+    return htmlspecialchars(trim($nombre), ENT_QUOTES, 'UTF-8');
 }
 
 function sanitizarEmail($email) {
@@ -11,17 +11,17 @@ function sanitizarEdad($edad) {
     return filter_var($edad, FILTER_SANITIZE_NUMBER_INT);
 }
 
-function sanitizarSitioWeb($sitioWeb) {
+function sanitizarSitio_Web($sitioWeb) {
     return filter_var(trim($sitioWeb), FILTER_SANITIZE_URL);
 }
 
 function sanitizarGenero($genero) {
-    return filter_var(trim($genero), FILTER_SANITIZE_STRING);
+    return htmlspecialchars(trim($genero), ENT_QUOTES, 'UTF-8');
 }
 
 function sanitizarIntereses($intereses) {
     return array_map(function($interes) {
-        return filter_var(trim($interes), FILTER_SANITIZE_STRING);
+        return htmlspecialchars(trim($interes), ENT_QUOTES, 'UTF-8');
     }, $intereses);
 }
 
