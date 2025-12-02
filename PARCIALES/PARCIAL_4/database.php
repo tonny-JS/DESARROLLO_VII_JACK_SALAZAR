d790c811c0dc3ffd72d0c5716c77ebde6e3da296
tonny-JS <jack.salazar@utp.ac.pa>
2025-12-01 20:23:37 -0600

    PARCIAL4- creacion de los archivos .php

--- PARCIALES/PARCIAL_4/database.php
+++ PARCIALES/PARCIAL_4/database.php
@@ -1,13 +1,12 @@
 <?php
-$host = "localhost";
-$user = "root";
-$pass = "90335177";
-$dbname = "techparts_db";
+define('DB_SERVER', 'localhost');
+define('DB_USERNAME', 'root');      // reemplaza con tu usuario MySQL
+define('DB_PASSWORD', '90335177');   // reemplaza con tu contraseña MySQL
+define('DB_NAME', 'techparts_db');        // nombre de la base de datos del parcial
 
-try {
-    $conn = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8", $user, $pass);
-    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-} catch (PDOException $e) {
-    die("Error de conexión: " . $e->getMessage());
+$conn = mysqli_connect(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_NAME);
+
+if($conn === false){
+    die("ERROR: No se pudo conectar. " . mysqli_connect_error());
 }
 ?>
